63ff6fb4573c148cf440b4747596eaf36482cd02
JustKillu <nightcorekillu@gmail.com>
2024-02-14 14:53:44 -0400

    nuevas rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage.jsx';
-
+import Register from './pages/Register.jsx'; 
+import Login from './pages/Login.jsx';
+import Navbar from './components/NavBar.jsx'; // Importa el componente Navbar
 
 function App() {
   return (
     <Router>
+      <Navbar /> 
       <Routes>
         <Route path="/" element={<HomePage />} />
-
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} /> 
       </Routes>
     </Router>
   );
